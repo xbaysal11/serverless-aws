f1da6d410467f1164cfcc5700547a360dc099514
xbaysal11 <baisalbek.daniiarov@gmail.com>
2024-01-25 15:24:12 +0600

    ADD download

--- src/functions/image/download/handler.ts
+++ src/functions/image/download/handler.ts
@@ -2,6 +2,8 @@ import 'source-map-support/register';
 import Image from '@/db/models/images';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3';
+const fs = require('fs');
+const https = require('https');
 
 const downloadImage = async (event) => {
   const createPresignedUrlWithClient = ({ region, bucket, key }) => {
@@ -9,6 +11,28 @@ const downloadImage = async (event) => {
     const command = new GetObjectCommand({ Bucket: bucket, Key: key });
     return getSignedUrl(client, command, { expiresIn: 100000 });
   };
+
+  async function fetchData(link) {
+    try {
+      const response = await fetch(link);
+      // Check if the request was successful (status code 200-299)
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      // Parse the response JSON
+      const imageBuffer = await response.blob();
+
+      // Now 'data' contains the parsed JSON data
+      console.log(imageBuffer);
+
+      // You can return the data if needed
+      return imageBuffer;
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+      // Handle the error as needed
+    }
+  }
 
   try {
     const fileId = event.queryStringParameters.fileid;
@@ -26,15 +50,38 @@ const downloadImage = async (event) => {
         bucket: 'aws-bucket-image-test',
         key: fileId,
       });
+
+      async function getImageBase64(url) {
+        return new Promise((resolve, reject) => {
+          https
+            .get(url, { responseType: 'arraybuffer' }, (response) => {
+              let data = Buffer.from([]);
+
+              response.on('data', (chunk) => {
+                data = Buffer.concat([data, chunk]);
+              });
+
+              response.on('end', () => {
+                const base64String = data.toString('base64');
+                resolve(base64String);
+              });
+            })
+            .on('error', (error) => {
+              reject(error);
+            });
+        });
+      }
+      const base64Data = await getImageBase64(clientUrl);
+
       return {
         statusCode: 200,
         headers: {
-          'Access-Control-Allow-Headers': 'Content-Type',
+          'Access-Control-Allow-Headers': '*',
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': '*',
           'Access-Control-Allow-Credentials': true,
         },
-        body: JSON.stringify({ imageRow, clientUrl }),
+        body: JSON.stringify({ imageRow, clientUrl, base64Data }),
       };
     } else {
       return {
